8d6a476a0dd091122f794504bba7a69a5c7ed966
Utsav Kafley <vastukafley@gmail.com>
2021-07-27 15:41:58 -0400

    Fix repeating park entries in viewing history

--- src/main.js
+++ src/main.js
@@ -6,13 +6,20 @@ import router from './router'
 export const store = createStore({
     state() {
         return {
-            viewHistory: []
+            viewHistory: [],
+            viewHistoryIdList: []
         }
     },
 
     mutations: {
         addToHistory(state, park) {
-            if (!state.viewHistory.includes(park)) state.viewHistory.push(park);
+            if (state.viewHistoryIdList.includes(park.id)) {
+                let index = state.viewHistoryIdList.indexOf(park.id)
+                state.viewHistoryIdList.splice(index, 1)
+                state.viewHistory.splice(index, 1)
+            }
+            state.viewHistoryIdList.push(park.id)
+            state.viewHistory.push(park)
         }
     },
 
